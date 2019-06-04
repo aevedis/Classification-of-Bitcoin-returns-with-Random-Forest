26e81d77a9fc2efa73080970b9a66c9a32a95015
Andrey E. Vedishchev <andrey.vedis@gmail.com>
2019-06-04 12:55:36 +0200

    Add files via upload
    At this point, wealth variation gets successfully represented in plotly.

--- backtest.py
+++ backtest.py
@@ -1,4 +1,12 @@
 import random
+import csv
+import datetime
+import cufflinks as cf
+import plotly
+import plotly.graph_objs as go
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+init_notebook_mode(connected=True)
+import plotly.plotly as py
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_curve, auc, accuracy_score
@@ -10,6 +18,10 @@ import pandas as pd
 import numpy as np
 import seaborn as sns; sns.set()
 import time
+
+# Authenticate with Plotly
+plotly.tools.set_credentials_file(username='AndreyVedis94',                                              
+                                  api_key='4JII7NXfEX71SWNuRGrO')
 
 # Read in data and display first 5 rows
 dataset = pd.read_csv('Data/dataset.csv')
@@ -46,33 +58,58 @@ print('Number of observations in the test data:',len(test))
 print("\n")
 
 
-money = []
-wealth = 100
-btc_amount = wealth / test['open'].iloc[0]
-print("Price of BTC at the moment of buying: " + str(test['open'].iloc[0]))
-print("\n")
-print("Amount of BTC bought: " + str(btc_amount))
+# Getting actual price for each day
+portfolio_values = []
+dates = []
 
-for i in test['pricevar'].index:
+# The two SLOC below are necessary because of the construction of the dataset
+num = num +1
+num2 = num2 +1
 
-	wealth = wealth + wealth * test['pricevar'][i]/100
-	money.append(wealth)
-
-#print(money)
-
-
-
-plt.plot(money)
-plt.xlabel('Dates')
-plt.ylabel('Price')
-plt.show()
-
-    
-
-
-
+dataset = pd.read_csv('Data/bitcoin_price_data.csv')
+df2 = pd.DataFrame(dataset)
+usd_holdings = 1000
+btc_holdings = usd_holdings / df2['open'][num]
+usd_holdings = 0
+portfolio_value = 0
+wealth = 0
+for row in range(len(df2)):
+    if row >= num and row <= num2:
+        date = df2['date'][row]
+        dates.append(date)
+        wealth = btc_holdings * df2['open'][row]
+        portfolio_values.append(wealth)
+    else:
+    	pass
 
 
+print(df['open'])
+#plot([go.Scatter(x=dates, y=portfolio_values, marker={'color': 'blue'})])
+
+#iplot([{"x": dates, "y": portfolio_values}]) # For Jupyter Notebook
+
+#iplot(cf.datagen.lines().iplot(asFigure=True, Plot with cufflinks
+#                               kind='scatter',xTitle='Dates',yTitle='Returns',title='Returns'))
 
 
+# Create a trace
+trace = go.Scatter(
+	name = 'Benchmark',
+    x = dates,
+    y = portfolio_values,
+    line = dict(
+        color = ('rgb(180, 46, 71)'),
+        width = 4,
+        dash = 'dot')
+)
 
+data = [trace]
+
+layout = dict(title = 'Bitcoin wealth variation in absolute prices',
+              xaxis = dict(title = 'Date'),
+              yaxis = dict(title = 'Wealth expressed in USD'),
+              )
+
+fig = dict(data=data, layout=layout)
+
+py.iplot(fig, filename='sysinv-bitcoin')
