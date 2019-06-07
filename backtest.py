0796d84c90349251dd7cd6b26c0092b24bb26cd7
Andrey E. Vedishchev <andrey.vedis@gmail.com>
2019-06-07 10:15:56 +0200

    Add files via upload
    Up do date scripts.

--- backtest.py
+++ backtest.py
@@ -25,9 +25,9 @@ plotly.tools.set_credentials_file(username='AndreyVedis94',
                                   api_key='4JII7NXfEX71SWNuRGrO')
 
 # Read in data and display first 5 rows
+test_size = 0.2
 dataset = pd.read_csv('Data/dataset.csv')
 df = pd.DataFrame(dataset)
-test_size = 0.2
 
 
 # Removing the first column (Date always increments, hence it is not useful for predicting)
@@ -53,25 +53,79 @@ train, test = df[df['is_test']==0], df[df['is_test']==1]
 train = train.drop('is_test', 1)
 test = test.drop('is_test', 1)
 
-# Show the number of observations for the test and training dataframes
-print('Number of observations in the training data:', len(train))
-print('Number of observations in the test data:',len(test))
+
+
+
+
+
+
+
+
+# Creating a list of the feature columns' names
+labels = df.columns[:25]
+
+# Assigning class' values to y variable
+y = train['class']
+X = df.drop('class', 1)
+
+my_X, my_y = X, df['class']
+
+start = time.time()
+
+# Create a random forest Classifier. By convention, clf means 'Classifier'
+clf = RandomForestClassifier(n_estimators = 600, max_depth = 180, max_features=15, random_state = 73)
+
+# Training the classifier on our test dataset
+clf.fit(train[labels], y)
+
+end = time.time()
+executiontime = end - start
+executiontimestr = str(executiontime)
+print("Random Forest successfully trained. The process took " + executiontimestr + " seconds")
+
+y_actual = test['class']
+y_predicted = clf.predict(test[labels])
+predictions = [round(value) for value in y_predicted]
+n_classes = 4
+yprob = clf.predict_proba(test[labels])
+accuracy = accuracy_score(y_actual, predictions)
+	
+#print(yprob)
 print("\n")
+print("Accuracy: %.2f%%" % (accuracy * 100.0))
+print("\n")
+print(pd.crosstab(y_actual, y_predicted, rownames=['Actual classes'], colnames=['Predicted classes']))
+
+# Plot ROC curve
+skplt.metrics.plot_roc(y_actual, yprob)
+plt.show()
+
+# Plot Learning curve
+#skplt.estimators.plot_learning_curve(clf, my_X, my_y, cv=5)
+#plt.show()
+
+# Plot Precision Recall Curve
+#skplt.metrics.plot_precision_recall_curve(y_actual, yprob)
+#plt.show()
+
+# Plot confusion matrix
+skplt.metrics.plot_confusion_matrix(y_true=y_actual, y_pred=y_predicted)
+plt.show()
+
 
 
 # Getting actual price for each day
 BH_portfolio_values = []
 actual_portfolio_values = []
 dates = []
-signals = []
-
+prices = []
 
 # The two SLOC below are necessary because of the construction of the dataset
 num = num +1
 num2 = num2 +1
 
 dataset = pd.read_csv('Data/bitcoin_price_data.csv')
-df2 = pd.DataFrame(dataset)
+df2 = pd.DataFrame(dataset) # size 1970
 USD_holdings = 1000
 BTC_holdings = 0
 benchmark_BTC_holdings = USD_holdings / df2['open'][num]
@@ -81,56 +135,70 @@ for row in range(len(df2)):
         date = df2['date'][row]
         dates.append(date)
 
-        # Generating random trading signals
-        signal = randint(1, 6)
+        price = df2['open'][row]
+        prices.append(price)
 
-        if signal == 1:
-            #Short 2x
-    	    if BTC_holdings > 0:
-                USD_holdings = BTC_holdings * df2['open'][row]
-                BTC_holdings = 0
-        elif signal == 2:
-    	    #Short 1.5x
-    	    if BTC_holdings > 0:
-                USD_holdings = BTC_holdings * df2['open'][row]
-                BTC_holdings = 0
-        elif signal == 3:
-    	    #Sell no lev
-    	    if BTC_holdings > 0:
-                USD_holdings = BTC_holdings * df2['open'][row]
-                BTC_holdings = 0
-        elif signal == 4:
-    	    #Buy no lev
-    	    if USD_holdings > 0:
-                BTC_holdings = USD_holdings / df2['open'][row]
-                USD_holdings = 0
-        elif signal == 5:
-    	    #Long 1.5x
-    	    if USD_holdings > 0:
-                BTC_holdings = USD_holdings / df2['open'][row]
-                USD_holdings = 0
-        elif signal == 6:
-    	    #Long 2x
-    	    if USD_holdings > 0:
-                BTC_holdings = USD_holdings / df2['open'][row]
-                USD_holdings = 0
 
-        wealth = BTC_holdings * df2['open'][row] + USD_holdings
-        actual_portfolio_values.append(wealth)
-
-        BH_wealth = benchmark_BTC_holdings * df2['open'][row] 
-        BH_portfolio_values.append(BH_wealth)
+       
     else:
     	pass
 
 
-print(df['open'])
+df3 = pd.DataFrame({'date': dates,
+'price': prices,
+})
+
+df3['prediction'] = y_predicted
+print(df3)
+
+
+for row in range(len(df3)):
+
+    if df3['prediction'][row] == 1:
+        #Short 2x
+        if BTC_holdings > 0:
+            USD_holdings = BTC_holdings * df3['price'][row]
+            BTC_holdings = 0
+    elif df3['prediction'][row] == 2:
+    	#Short 1.5x
+        if BTC_holdings > 0:
+            USD_holdings = BTC_holdings * df3['price'][row]
+            BTC_holdings = 0
+    elif df3['prediction'][row] == 3:
+    	#Sell no lev
+        if BTC_holdings > 0:
+            USD_holdings = BTC_holdings * df3['price'][row]
+            BTC_holdings = 0
+    elif df3['prediction'][row] == 4:
+    	#Buy no lev
+        if USD_holdings > 0:
+            BTC_holdings = USD_holdings / df3['price'][row]
+            USD_holdings = 0
+    elif df3['prediction'][row] == 5:
+    	#Long 1.5x
+        if USD_holdings > 0:
+            BTC_holdings = USD_holdings / df3['price'][row]
+            USD_holdings = 0
+    elif df3['prediction'][row] == 6:
+    	#Long 2x
+        if USD_holdings > 0:
+            BTC_holdings = USD_holdings / df3['price'][row]
+            USD_holdings = 0
+
+    wealth = BTC_holdings * df3['price'][row] + USD_holdings
+    actual_portfolio_values.append(wealth)
+
+    BH_wealth = benchmark_BTC_holdings * df3['price'][row] 
+    BH_portfolio_values.append(BH_wealth)
+
 #plot([go.Scatter(x=dates, y=portfolio_values, marker={'color': 'blue'})])
 
 #iplot([{"x": dates, "y": portfolio_values}]) # For Jupyter Notebook
 
 #iplot(cf.datagen.lines().iplot(asFigure=True, Plot with cufflinks
 #                               kind='scatter',xTitle='Dates',yTitle='Returns',title='Returns'))
+
+print(actual_portfolio_values)
 
 
 # Create a trace
@@ -151,8 +219,7 @@ traceportfolio = go.Scatter(
     name = 'Portfolio',
     line = dict(
         color = ('rgb(45, 78, 175)'),
-        width = 4,
-        dash = 'dot')
+        width = 4)
 )
 
 data = [tracebench, traceportfolio]
--- classifier.py
+++ classifier.py
@@ -1,5 +1,6 @@
 import random
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_curve, auc, accuracy_score
 from sklearn.datasets import load_digits
@@ -13,21 +14,15 @@ import time
 # Read in data and display first 5 rows
 dataset = pd.read_csv('Data/dataset.csv')
 df = pd.DataFrame(dataset)
-#df = pd.to_numeric(df)
-#df = df.astype(np.float16)
-#df=df.apply(pd.to_numeric, errors='coerce')
 
 
 # Removing the first column (Date always increments, hence it is not useful for predicting)
 df = df.drop('date', 1)
 
-# Printing descriptive statistics about the dataset
-#print(df.describe())
-
-
+# Splitting dataset into train and test sets
 train, test = train_test_split(df, test_size=0.2)
 
-# Creating a list of the feature column's names
+# Creating a list of the feature columns' names
 labels = df.columns[:25]
 
 # Assigning class' values to y variable
@@ -39,7 +34,7 @@ my_X, my_y = X, df['class']
 start = time.time()
 
 # Create a random forest Classifier. By convention, clf means 'Classifier'
-clf = RandomForestClassifier(n_estimators = 600, random_state = 73)
+clf = RandomForestClassifier(n_estimators = 600, max_depth = 180, max_features=15, random_state = 73)
 
 # Training the classifier on our test dataset
 clf.fit(train[labels], y)
@@ -63,13 +58,14 @@ print("\n")
 print(pd.crosstab(y_actual, y_predicted, rownames=['Actual classes'], colnames=['Predicted classes']))
 
 
-# Plot Learning curve
-#skplt.estimators.plot_learning_curve(clf, my_X, my_y, cv=5)
-#plt.show()
 
 # Plot ROC curve
 skplt.metrics.plot_roc(y_actual, yprob)
 plt.show()
+
+# Plot Learning curve
+#skplt.estimators.plot_learning_curve(clf, my_X, my_y, cv=5)
+#plt.show()
 
 # Plot Precision Recall Curve
 #skplt.metrics.plot_precision_recall_curve(y_actual, yprob)
@@ -78,3 +74,4 @@ plt.show()
 # Plot confusion matrix
 skplt.metrics.plot_confusion_matrix(y_true=y_actual, y_pred=y_predicted)
 plt.show()
+
