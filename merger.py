76d50cacebf4e9ff3a3ab09c20f17fa2bce32717
Andrey E. Vedishchev <andrey.vedis@gmail.com>
2019-07-29 09:13:28 +0200

    Add files via upload

---
+++ merger.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+dataleft = pd.read_csv('dataset2.csv')
+dataright = pd.read_csv('dataset2.csv')
+
+left = pd.DataFrame(dataleft)    
+right = pd.DataFrame(dataright)
+
+
+df = pd.DataFrame(dataset)
